863b55fb15d497e9ef978daa170875b263ab20b8
ersintunbul <ersintunbul@hotmail.com>
2021-07-04 23:06:55 +0300

    code first context added and first migration and database creation completed

---
+++ LibraryWebApplication/Data.Access.Layer/LibraryWebApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Data.Access.Layer.Classes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Access.Layer
+{
+    public class LibraryWebApplicationContext : DbContext
+    {
+        public LibraryWebApplicationContext()
+        {
+        }
+        public DbSet<Books> Books { get; set; }
+        public DbSet<Members> Members { get; set; }
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Types> Types { get; set; }
+        public DbSet<BookTransactions> BookTransactions { get; set; }
+        public DbSet<BookTypes> BookTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server =.\SQLEXPRESS; Database = LibraryWebApplicationDB; Trusted_Connection = True;");
+        }
+    }
+}
---
+++ LibraryWebApplication/Data.Access.Layer/Migrations/20210704200129_LibraryWebApplicationMigrations.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Access.Layer.Migrations
+{
+    public partial class LibraryWebApplicationMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar", nullable: true),
+                    Surname = table.Column<string>(type: "varchar", nullable: true),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar", nullable: true),
+                    Surname = table.Column<string>(type: "varchar", nullable: true),
+                    MembershipDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "varchar", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar", nullable: true),
+                    LoanDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StockQuantity = table.Column<int>(type: "int", nullable: false),
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookTransactions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookID = table.Column<int>(type: "int", nullable: false),
+                    MemberID = table.Column<int>(type: "int", nullable: false),
+                    BorrowingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTransactions", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BookTransactions_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookTransactions_Members_MemberID",
+                        column: x => x.MemberID,
+                        principalTable: "Members",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookTypes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeID = table.Column<int>(type: "int", nullable: false),
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTypes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BookTypes_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookTypes_Types_TypeID",
+                        column: x => x.TypeID,
+                        principalTable: "Types",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorID",
+                table: "Books",
+                column: "AuthorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTransactions_BookID",
+                table: "BookTransactions",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTransactions_MemberID",
+                table: "BookTransactions",
+                column: "MemberID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTypes_BookID",
+                table: "BookTypes",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTypes_TypeID",
+                table: "BookTypes",
+                column: "TypeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookTransactions");
+
+            migrationBuilder.DropTable(
+                name: "BookTypes");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
